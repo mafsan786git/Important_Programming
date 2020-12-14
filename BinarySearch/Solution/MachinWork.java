e2a176d7f8d9b4d40b9d7fafb299134528f773b6
mafsan786git <mafsan78692@gmail.com>
2020-12-14 11:21:40 +0530

    added more

--- BinarySearch/Solution/MachinWork.java
+++ BinarySearch/Solution/MachinWork.java
@@ -1,3 +1,5 @@
+package Solution;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- BinarySearch/Solution/WoodCutter.java
+++ BinarySearch/Solution/WoodCutter.java
@@ -1,3 +1,5 @@
+package Solution;
+
 import java.io.*;
 import java.util.Arrays;
 
